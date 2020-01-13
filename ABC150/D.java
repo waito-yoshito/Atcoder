c3413b75ccedb592ce620a1e0c67f3c4cb127d37
waito <waito_ss@icloud.com>
2020-01-14 05:34:44 +0900

    D問題

---
+++ ABC150/D.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+class D{
+    public static void main(String args[])throws IOException{
+      try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+        
+      }
+    }
+}
